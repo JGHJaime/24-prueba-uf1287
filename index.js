d6a827b2807916d0b600de4cb364b82cbca3edd3
JghJaime <palomojr01@gmail.com>
2024-06-18 13:20:21 +0200

    cambio y añadido de endpoints

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const port = 3000
 
 app.use(express.static('/public'));
 
+app.get('/suma', (req, res) => {
+  
+  res.send('Suma')
+})
 app.get('/hola', (req, res) => {
   
   res.send('Hello World!')
 })
-
+app.get('/multiplicacion', (req, res) => {
+  
+  res.send('multiplica!')
+})
+app.get('/lista-de-1', (req, res) => {
+  
+  res.send('lista')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
