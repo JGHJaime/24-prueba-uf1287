2e5250fd257738a1b9c0ef1f2998bca158c603f3
JghJaime <palomojr01@gmail.com>
2024-06-18 10:53:39 +0200

    ponemos el estatico

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/a', (req, res) => {
+app.get('/hola', (req, res) => {
   res.send('Hello World!')
 })
 
